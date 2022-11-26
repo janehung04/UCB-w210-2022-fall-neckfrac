12848a958ea83eebee3313b09f74d6a1317230a5
Jane Hung <janehung@berkeley.edu>
2022-11-26 16:25:51 -0500

    add vertebrae analysis for shifting and number of frac

--- error_helper.py
+++ error_helper.py
@@ -20,7 +20,6 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import random
 from glob import glob
-from IPython.display import display
 
 pd.set_option("mode.chained_assignment", None)
 
@@ -164,8 +163,14 @@ def eval_model(df):
     return output
 
 
-def get_vertebrae_crosstab(model_results):
-
+def format_vertebrae_analysis(model_results):
+    """
+    Format model results to just look at vertebrae in this format:
+           Unnamed: 0                     row_id  actual  fractured vertebrae actual_vertebrae predicted_vertebrae
+               0  1.2.826.0.1.3680043.10001       0          0        C1             None                None
+               1  1.2.826.0.1.3680043.10001       0          0        C2             None                None
+               2  1.2.826.0.1.3680043.10001       0          0        C3             None                None
+    """
     model_results_vert = model_results[
         ~model_results.row_id.str.contains("patient_overall")
     ].copy()
@@ -186,6 +191,15 @@ def get_vertebrae_crosstab(model_results):
         "predicted_vertebrae",
     ] = model_results_vert.loc[model_results_vert["fractured"] == 1, "vertebrae"]
 
+    output_fname = "fmt_raw_vertebrae.csv"
+    model_results_vert.to_csv(output_fname)
+    print(f"Evaluation results stored in {output_fname}")
+    return model_results_vert
+
+
+def get_vertebrae_crosstab(model_results):
+    model_results_vert = format_vertebrae_analysis(model_results)
+
     crosstab_predict = pd.merge(
         model_results_vert[model_results_vert.actual_vertebrae.notnull()]
         .groupby("row_id")
@@ -204,6 +218,108 @@ def get_vertebrae_crosstab(model_results):
         .map(lambda x: "{:.2f}%".format(x * 100))
         .reset_index(name="proportion")
     )
+
+
+def get_shifts_in_predictions(model_results):
+    model_results_vert = format_vertebrae_analysis(model_results=model_results)
+
+    # Consider only matches where there was a fracture and the model detected it
+    crosstab_lst = pd.merge(
+        model_results_vert[model_results_vert.actual_vertebrae.notnull()]
+        .groupby("row_id")
+        .agg({"actual_vertebrae": list})
+        .reset_index(),
+        model_results_vert[model_results_vert.predicted_vertebrae.notnull()]
+        .groupby("row_id")
+        .agg({"predicted_vertebrae": list})
+        .reset_index(),
+        how="inner",
+    )
+
+    # Are inferior vertebrae always predicted?
+    # ex. predicts c7 instead of c1
+    crosstab_lst["all_pred_inferior"] = crosstab_lst.apply(
+        lambda row: all(
+            actual_el < pred_el
+            for actual_el in row["actual_vertebrae"]
+            for pred_el in row["predicted_vertebrae"]
+        ),
+        axis=1,
+    )
+    print(crosstab_lst["all_pred_inferior"].value_counts(normalize=True))
+
+    crosstab_lst["any_pred_inferior"] = crosstab_lst.apply(
+        lambda row: any(
+            actual_el < pred_el
+            for actual_el in row["actual_vertebrae"]
+            for pred_el in row["predicted_vertebrae"]
+        ),
+        axis=1,
+    )
+    print(crosstab_lst["any_pred_inferior"].value_counts(normalize=True))
+
+    # Are superior vertebrae always predicted?
+    # ex. predicts c1 instead of c7
+    crosstab_lst["all_pred_superior"] = crosstab_lst.apply(
+        lambda row: all(
+            actual_el > pred_el
+            for actual_el in row["actual_vertebrae"]
+            for pred_el in row["predicted_vertebrae"]
+        ),
+        axis=1,
+    )
+    print(crosstab_lst["all_pred_superior"].value_counts(normalize=True))
+
+    crosstab_lst["any_pred_superior"] = crosstab_lst.apply(
+        lambda row: any(
+            actual_el > pred_el
+            for actual_el in row["actual_vertebrae"]
+            for pred_el in row["predicted_vertebrae"]
+        ),
+        axis=1,
+    )
+    print(crosstab_lst["any_pred_superior"].value_counts(normalize=True))
+
+
+def get_fracture_counts(model_results):
+    model_results_vert = format_vertebrae_analysis(model_results=model_results)
+
+    # Consider only matches where there was a fracture and the model detected it
+    crosstab_lst = pd.merge(
+        model_results_vert[model_results_vert.actual_vertebrae.notnull()]
+        .groupby("row_id")
+        .agg({"actual_vertebrae": list})
+        .reset_index(),
+        model_results_vert[model_results_vert.predicted_vertebrae.notnull()]
+        .groupby("row_id")
+        .agg({"predicted_vertebrae": list})
+        .reset_index(),
+        how="outer",
+    )
+    crosstab_lst.loc[
+        ~crosstab_lst.actual_vertebrae.isna(), "actual_vertebrae"
+    ] = crosstab_lst.loc[
+        ~crosstab_lst.actual_vertebrae.isna(), "actual_vertebrae"
+    ].apply(
+        lambda x: len(x)
+    )
+    crosstab_lst.loc[
+        ~crosstab_lst.predicted_vertebrae.isna(), "predicted_vertebrae"
+    ] = crosstab_lst.loc[
+        ~crosstab_lst.predicted_vertebrae.isna(), "predicted_vertebrae"
+    ].apply(
+        lambda x: len(x)
+    )
+    crosstab_lst = crosstab_lst.fillna(0)
+
+    for el in range(crosstab_lst["actual_vertebrae"].nunique()):
+        print(f"\n\nProcessing {el} actual fractures...")
+        print(
+            crosstab_lst.loc[
+                crosstab_lst.actual_vertebrae == el, "predicted_vertebrae"
+            ].describe()
+        )
+        
 
 
 def get_sagittal_view(bad_patients):
@@ -263,18 +379,26 @@ def get_worst_patients(model_results):
 
 
 # Get overall model performance
-def get_model_results(fname,verbose=True):
+def get_model_results(fname, verbose=True):
 
     # CHECKPOINT_PATH='./densenet121_baseline_best_fold0_2a1ca668-540d-11ed-abb4-aa0cb2e0f96a.pth'
     # model_results = pd.read_csv(f'./{CHECKPOINT_PATH[2:-4]}_train_results.csv')
+
+    # TODO debug fname = "full_model_validation/valid_folds_all_fmt.csv"
     model_results = pd.read_csv(fname)
 
     if verbose:
         print(eval_model(model_results))
         print(get_vertebrae_crosstab(model_results))
+        print(get_shifts_in_predictions(model_results))
 
-    eval_model(model_results).to_csv("eval_model.csv")
-    get_vertebrae_crosstab(model_results).to_csv("vertebrae_crosstab.csv")
+    output_fname = "eval_model.csv"
+    eval_model(model_results).to_csv(output_fname)
+    print(f"Evaluation results stored in {output_fname}")
+
+    output_fname = "vertebrae_crosstab.csv"
+    get_vertebrae_crosstab(model_results).to_csv(output_fname)
+    print(f"Vertebrae crosstab results stored in {output_fname}")
 
     try:
         get_worst_patients(model_results)
--- eval_cnn25.py
+++ eval_cnn25.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 
 import importlib
-
 importlib.reload(error_helper)
 
 
